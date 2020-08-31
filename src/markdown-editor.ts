b5d3d8bd352a6de4d1ed3f0f0aaaa5da09d0fcea
Lenard Funk <lenard.funk@uni-ulm.de>
2020-08-31 22:46:08 +0200

    feat: add ts-doc and comments

--- src/markdown-editor.ts
+++ src/markdown-editor.ts
@@ -20,23 +20,40 @@ export class MarkdownEditor {
     CodeMirror.fromTextArea(textarea, { mode: 'gfm' });
   }
 
+  /**
+   * Toggles "bold" for each selection.
+   */
   public toggleBold() {
-    this.toggleBlock('**');
+    this.toggleInlineFormatting('**');
   }
 
+  /**
+   * Toggles "italic" for each selection.
+   */
   public toggleItalic() {
-    this.toggleBlock('*');
+    this.toggleInlineFormatting('*');
   }
 
+  /**
+   * Toggles "strikethrough" for each selection.
+   */
   public toggleStrikethrough() {
-    this.toggleBlock('~~');
+    this.toggleInlineFormatting('~~');
   }
 
+  /**
+   * Toggles "inline code" for each selection.
+   */
   public toggleInlineCode() {
-    this.toggleBlock('`');
+    this.toggleInlineFormatting('`');
   }
 
-  private toggleBlock(op: string) {
+  /**
+   * Toggles inline formatting for each selection by wrapping and unwrapping each selection
+   * with specified operator.
+   * @param op the operator
+   */
+  private toggleInlineFormatting(op: string) {
     const newSelections: CodeMirror.Range[] = [];
     let currentShift = 0; // indicates how many operators have been inserted and deleted
     for (const sel of this.cm.listSelections()) {
@@ -67,30 +84,42 @@ export class MarkdownEditor {
 
       if (hasPrefixToken) {
         this.cm.replaceRange('', prefixStart, from, '+toggleBlock');
+        currentShift--;
+
+        // Adjust selections to originally selected characters
         selection.anchor.ch -= op.length;
         if (!selection.empty()) selection.head.ch -= op.length;
-        currentShift--;
       } else {
         this.cm.replaceRange(op, from, undefined, '+toggleBlock');
+        currentShift++;
+
+        // Adjust selections to originally selected characters
         selection.anchor.ch += op.length;
         if (!selection.empty()) selection.head.ch += op.length;
-        currentShift++;
       }
 
-      newSelections.push(selection); // Adjust selections to originally selected characters
+      newSelections.push(selection);
     }
 
     this.cm.setSelections(newSelections, undefined, { origin: '+toggleBlock' });
     this.cm.focus();
   }
 
+  /**
+   * Sets the specified heading level for each selected line. If `level` is 0, the heading token is removed.
+   * @param level the heading level
+   */
   public setHeadingLevel(level: 0 | 1 | 2 | 3 | 4 | 5 | 6) {
     const headingToken = '#'.repeat(level) + (level === 0 ? '' : ' ');
     this.replaceTokenAtLineStart((oldLineContent) => oldLineContent.replace(/^((#)*( )?)/, headingToken));
   }
 
+  /**
+   * Toggles "quote" for each selected line.
+   */
   public toggleQuote() {
     this.replaceTokenAtLineStart((oldLineContent) => {
+      // Has selected line a quote token?
       if (oldLineContent.search(/^>(\t| )*/) === -1) {
         return '> ' + oldLineContent;
       } else {
@@ -99,10 +128,16 @@ export class MarkdownEditor {
     });
   }
 
+  /**
+   * Toggles "unordered list" for each selected line. Furthermore, a selected ordered list line is
+   * transformed to an unordered list.
+   */
   public toggleUnorderedList() {
     this.replaceTokenAtLineStart((oldLineContent) => {
+      // Has selected line a bullet point token?
       if (oldLineContent.search(MarkdownEditor.UNORDERED_LIST_PATTERN) === -1) {
         const token = '- ';
+        // Has selected line an enumeration token?
         if (oldLineContent.search(MarkdownEditor.ORDERED_LIST_PATTERN) === -1) {
           return token + oldLineContent;
         } else {
@@ -114,11 +149,19 @@ export class MarkdownEditor {
     });
   }
 
+  /**
+   * Toggles "ordered list" for each selected line. Furthermore, a selected unordered list line is
+   * transformed to an ordered list. Additionally adjusts the subsequent lines that are connected
+   * to the list of the selected line.
+   */
   public toggleOrderedList() {
     this.replaceTokenAtLineStart((oldLineContent, lineNumber) => {
+      // Has selected line an enumeration token?
       if (oldLineContent.search(MarkdownEditor.ORDERED_LIST_PATTERN) === -1) {
         const prevLine = this.cm.getLine(lineNumber - 1);
         let listNumber: number;
+
+        // Is previous line already enumerated list? -> Determine enumeration token for selected line
         if (!prevLine || prevLine.search(MarkdownEditor.ORDERED_LIST_PATTERN) === -1) {
           listNumber = 1;
         } else {
@@ -127,6 +170,8 @@ export class MarkdownEditor {
         }
         this.processNextLinesOfOrderedList(lineNumber, listNumber);
         const numberToken = listNumber + '. ';
+
+        // Has selected line a bullet point token?
         if (oldLineContent.search(MarkdownEditor.UNORDERED_LIST_PATTERN) === -1) {
           return numberToken + oldLineContent;
         } else {
@@ -139,6 +184,11 @@ export class MarkdownEditor {
     });
   }
 
+  /**
+   * Adjusts enumeration of subsequent lines in same ordered list as line *baseLineNumber*.
+   * @param baseLineNumber the selected line which is toggled
+   * @param baseListNumber the list number of the selected line (should be 0, if list starts after selected line)
+   */
   private processNextLinesOfOrderedList(baseLineNumber: number, baseListNumber: number) {
     let listNumber = baseListNumber;
     let nextLineNumber = baseLineNumber + 1;
@@ -156,6 +206,11 @@ export class MarkdownEditor {
     }
   }
 
+  /**
+   * Replaces each selected line with the result of the callback function `replaceFn`.
+   * Additionally adjusts the selection boundaries to the originally selected boundaries.
+   * @param replaceFn callback function to the calculate the line replacements
+   */
   private replaceTokenAtLineStart(replaceFn: (oldLineContent: string, lineNumber: number) => string) {
     const newSelections: CodeMirror.Range[] = [];
     for (const sel of this.cm.listSelections()) {
@@ -172,6 +227,7 @@ export class MarkdownEditor {
           '+replaceTokenAtLineStart'
         );
 
+        // Set shifts for selection start and end
         if (lineNumber === selection.from().line) {
           shiftFrom = newLineContent.length - oldLineContent.length;
         }
@@ -179,6 +235,7 @@ export class MarkdownEditor {
           shiftTo = newLineContent.length - oldLineContent.length;
         }
       }
+      // Adjust selection boundaries to originally selected boundaries
       if (_.isEqual(selection.anchor, selection.from())) {
         selection.anchor.ch += shiftFrom;
         if (!selection.empty()) selection.head.ch += shiftTo;
@@ -193,6 +250,9 @@ export class MarkdownEditor {
     this.cm.focus();
   }
 
+  /**
+   * Wraps each selection with code block tokens, which are inserted in separate lines.
+   */
   public insertCodeBlock() {
     const newSelections: CodeMirror.Range[] = [];
     const selections = _.cloneDeep(this.cm.listSelections());
