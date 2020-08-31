239eabca2b2bc2b3d4bf52cbffbeb15de75e2278
Lenard Funk <lenard.funk@uni-ulm.de>
2020-08-31 13:13:54 +0200

    feat: add `toggleInlineCode` and `toggleOrderedList`

--- src/markdown-editor.ts
+++ src/markdown-editor.ts
@@ -3,6 +3,9 @@ require('codemirror/mode/gfm/gfm.js');
 import _ from 'lodash';
 
 export class MarkdownEditor {
+  public static readonly ORDERED_LIST_PATTERN = /^(\d)+\.(\t| )+/;
+  public static readonly UNORDERED_LIST_PATTERN = /^(\*|-)(\t| )+/;
+
   private cm: CodeMirror.Editor;
   private cmOptions: CodeMirror.EditorConfiguration;
 
@@ -27,6 +30,10 @@ export class MarkdownEditor {
 
   public toggleStrikethrough() {
     this.toggleBlock('~~');
+  }
+
+  public toggleInlineCode() {
+    this.toggleBlock('`');
   }
 
   private toggleBlock(op: string) {
@@ -93,22 +100,63 @@ export class MarkdownEditor {
   }
 
   public toggleUnorderedList() {
-    const orderedListPattern = /^(\d)+\.(\t| )+/;
-    const unorderedListPattern = /^(\*|-)(\t| )+/;
     this.replaceTokenAtLineStart((oldLineContent) => {
-      if (oldLineContent.search(unorderedListPattern) === -1) {
-        if (oldLineContent.search(orderedListPattern) === -1) {
-          return '- ' + oldLineContent;
+      if (oldLineContent.search(MarkdownEditor.UNORDERED_LIST_PATTERN) === -1) {
+        const token = '- ';
+        if (oldLineContent.search(MarkdownEditor.ORDERED_LIST_PATTERN) === -1) {
+          return token + oldLineContent;
         } else {
-          return oldLineContent.replace(orderedListPattern, '- ');
+          return oldLineContent.replace(MarkdownEditor.ORDERED_LIST_PATTERN, token);
         }
       } else {
-        return oldLineContent.replace(unorderedListPattern, '');
+        return oldLineContent.replace(MarkdownEditor.UNORDERED_LIST_PATTERN, '');
       }
     });
   }
 
-  private replaceTokenAtLineStart(replaceFn: (oldLineContent: string) => string) {
+  public toggleOrderedList() {
+    this.replaceTokenAtLineStart((oldLineContent, lineNumber) => {
+      if (oldLineContent.search(MarkdownEditor.ORDERED_LIST_PATTERN) === -1) {
+        const prevLine = this.cm.getLine(lineNumber - 1);
+        let listNumber: number;
+        if (!prevLine || prevLine.search(MarkdownEditor.ORDERED_LIST_PATTERN) === -1) {
+          listNumber = 1;
+        } else {
+          const dotPos = prevLine.search(/\./);
+          listNumber = +prevLine.substring(0, dotPos) + 1;
+        }
+        this.processNextLinesOfOrderedList(lineNumber, listNumber);
+        const numberToken = listNumber + '. ';
+        if (oldLineContent.search(MarkdownEditor.UNORDERED_LIST_PATTERN) === -1) {
+          return numberToken + oldLineContent;
+        } else {
+          return oldLineContent.replace(MarkdownEditor.UNORDERED_LIST_PATTERN, numberToken);
+        }
+      } else {
+        this.processNextLinesOfOrderedList(lineNumber, 0);
+        return oldLineContent.replace(MarkdownEditor.ORDERED_LIST_PATTERN, '');
+      }
+    });
+  }
+
+  private processNextLinesOfOrderedList(baseLineNumber: number, baseListNumber: number) {
+    let listNumber = baseListNumber;
+    let nextLineNumber = baseLineNumber + 1;
+    let nextLine = this.cm.getLine(nextLineNumber);
+    while (nextLine && nextLine.search(MarkdownEditor.ORDERED_LIST_PATTERN) !== -1) {
+      const listNumberString = `${++listNumber}`;
+      const dotPos = nextLine.search(/\./);
+      this.cm.replaceRange(
+        listNumberString,
+        { line: nextLineNumber, ch: 0 },
+        { line: nextLineNumber, ch: dotPos },
+        '+replaceTokenAtLineStart'
+      );
+      nextLine = this.cm.getLine(++nextLineNumber);
+    }
+  }
+
+  private replaceTokenAtLineStart(replaceFn: (oldLineContent: string, lineNumber: number) => string) {
     const newSelections: CodeMirror.Range[] = [];
     for (const sel of this.cm.listSelections()) {
       const selection = _.cloneDeep(sel);
@@ -116,12 +164,12 @@ export class MarkdownEditor {
       let shiftTo = 0;
       for (let lineNumber = selection.from().line; lineNumber <= selection.to().line; lineNumber++) {
         const oldLineContent = this.cm.getLine(lineNumber);
-        const newLineContent = replaceFn(oldLineContent);
+        const newLineContent = replaceFn(oldLineContent, lineNumber);
         this.cm.replaceRange(
           newLineContent,
           { line: lineNumber, ch: 0 },
           { line: lineNumber, ch: oldLineContent.length },
-          '+setHeadingLevel'
+          '+replaceTokenAtLineStart'
         );
 
         if (lineNumber === selection.from().line) {
