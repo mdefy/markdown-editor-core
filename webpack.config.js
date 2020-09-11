f8d66fa825c741504e44761588f7653891e58e76
Lenard Funk <lenard.funk@uni-ulm.de>
2020-09-11 13:20:22 +0200

    build: configure webpack correctly + use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  // devServer: {
-  //   contentBase: path.join(__dirname, 'dist'),
-  // },
   module: {
     rules: [
       {
-        test: /\.ts?$/,
+        test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.ts', '.js'],
+    extensions: ['.ts', '.js'],
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
+  },
+  optimization: {
+    minimize: true,
   },
 };
