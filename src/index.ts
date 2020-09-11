f94cfd6fbdbde745b3d3158e518ae9b7e788a5b2
Lenard Funk <lenard.funk@uni-ulm.de>
2020-09-11 14:08:33 +0200

    refactor: improve test UI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import { MarkdownEditor } from './markdown-editor';
 
-const mde = new MarkdownEditor(document.body);
-
-document.getElementById('testButton')?.addEventListener('click', () => mde.toggleBold());
+const mde = new MarkdownEditor(document.getElementById('editor')!);
