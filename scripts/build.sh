ba0b0ab0d20f6958e2a1b8b17427684f0ad38e02
Lenard Funk <lenard.funk@uni-ulm.de>
2020-10-21 20:01:16 +0200

    chore: move "build" and "pack" scripts to own files

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+LIB_ROOT=./lib
+LIB_DIST=$LIB_ROOT/dist
+LIB_PACKAGE=./lib/package.json
+LIB_README=./lib/README.md
+LIB_DIST_PACKAGE=./lib/dist/package.json
+LIB_DIST_README=./lib/dist/README.md
+
+if [ -d "$LIB_DIST" ]; then
+  echo "Remove $LIB_DIST"
+  rm "$LIB_DIST" -r;
+fi
+  
+echo "Compile $LIB_ROOT to $LIB_DIST"
+tsc -p ./lib
+
+echo "Copy $LIB_PACKAGE and $LIB_README to $LIB_DIST"
+cp $LIB_PACKAGE $LIB_DIST_PACKAGE
+cp $LIB_README $LIB_DIST_README
+
+echo "Done!"
---
+++ scripts/pack.sh
@@ -0,0 +1,4 @@
+echo "Pack ./lib/dist to ./build\n"
+
+cd ./build
+npm pack ../lib/dist
