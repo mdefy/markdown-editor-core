630efc4ad8f444930cc113dcd7094f84041aac40
Lenard Funk <lenard.funk@uni-ulm.de>
2020-12-16 14:11:51 +0100

    chore: improve shell scripts

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Pack ./lib/dist to ./build\n"
+{
+  echo "Pack ./lib/dist to ./build"
 
-cd ./build
-npm pack ../lib/dist
+  mkdir build
+  cd ./build
+  npm pack ../lib/dist
+
+} || {
+  read -p "Press Enter to finish..."
+}
