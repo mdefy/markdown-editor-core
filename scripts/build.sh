f8ea0bdd76f87efaefed60f883b29cdb2eaa3284
Lenard Funk <lenard.funk@uni-ulm.de>
2021-01-20 05:34:27 +0100

    chore: add license to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,12 @@
 {
   LIB_ROOT=./lib
   LIB_DIST=$LIB_ROOT/dist
-  LIB_PACKAGE=./lib/package.json
-  LIB_README=./lib/README.md
-  LIB_DIST_PACKAGE=./lib/dist/package.json
-  LIB_DIST_README=./lib/dist/README.md
+  LIB_PACKAGE=$LIB_ROOT/package.json
+  LIB_README=./README.md
+  LIB_LICENSE=./LICENSE
+  LIB_DIST_PACKAGE=$LIB_ROOT/dist/package.json
+  LIB_DIST_README=$LIB_ROOT/dist/README.md
+  LIB_DIST_LICENSE=$LIB_ROOT/dist/LICENSE
 
   if [ -d "$LIB_DIST" ]; then
     echo "Remove $LIB_DIST"
@@ -19,6 +21,7 @@
   echo "Copy $LIB_PACKAGE and $LIB_README to $LIB_DIST"
   cp $LIB_PACKAGE $LIB_DIST_PACKAGE
   cp $LIB_README $LIB_DIST_README
+  cp $LIB_LICENSE $LIB_DIST_LICENSE
 
   echo "Done!"
 
